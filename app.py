e06d26272aa2066f5e268f9d01eff16ad4c347d4
unknown <limteikyee@gmail.com>
2019-02-21 19:09:01 +0800

    change html name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
