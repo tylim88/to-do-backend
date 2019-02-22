d872c0753dc36551ccc851b8acc02acba3f64bf5
unknown <limteikyee@gmail.com>
2019-02-22 16:47:38 +0800

    serve service worker file

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 # This is app.py, this is the main file called.
 from src import app
-from flask import render_template
+from flask import render_template, send_from_directory
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/static/<name>')
-# def root(name):
-#     return app.send_static_file(name)
+@app.route('/service-worker.js')
+def worker():
+    # the directory path is relative to path where app is created
+    return send_from_directory('build','service-worker.js')
+
+@app.route('/<name>')
+def others(name):
+    return send_from_directory('build',name)
 
 if __name__ == '__main__':
     app.run()
